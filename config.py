be00b56e49615c4a5a25870b80c83a0199e77186
Veronika Maklakova <nikamaklakova@gmail.com>
2021-01-10 19:31:04 +0600

    db connection cred

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
 import psycopg2
 
-CONECTIONPG = psycopg2.connect(dbname='postgres', user='postgres',
-                            password='postgres', host='localhost', port='5433')
+# CONECTIONPG = psycopg2.connect(dbname='postgres', user='postgres',
+#                             password='postgres', host='localhost', port='5433')
+
+
+CONECTIONPG = psycopg2.connect(
+    dbname='d85ademi0g2jkj',
+    user='biyeyfpgadkcij',
+    password='e3340e7b58d6897c4d21e511185da83ac457b35c6f0bbcd3573a23fc6c9fbc77',
+    host='ec2-54-73-90-195.eu-west-1.compute.amazonaws.com',
+    port='5432',
+)
